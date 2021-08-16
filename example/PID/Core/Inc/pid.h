a069774d42a492316e87b0242a7e1e802417ba79
qitas <qitas@qitas.cn>
2021-08-16 13:32:50 +0800

    init

---
+++ example/PID/Core/Inc/pid.h
@@ -0,0 +1,86 @@
+/**
+ ******************************************************************************
+ * @file     : pid.h
+ * @brief    : This file contains all the function prototypes for
+ *             the pid.c file
+ * @version  : v2.0.1
+ * @author   : sin1111yi
+ * @other    : All algorithms reference -- https://blog.csdn.net/qq_18454025/article/details/103443771 --
+ ******************************************************************************
+ */
+#ifndef __PID_H
+#define __PID_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define PID_DEFAULT_ACCURACY    0.0005
+
+#define PID_DISABLE             0x00
+#define PID_ENABLE              0x01
+#define PID_UNINIT              0x02
+#define PID_FAILED              0x03
+
+#define Output               Uk
+#define PID_RESET_OUPUT      0x00
+#define PID_HOLD_OUTPUT      0x01
+
+typedef enum __PID_StatusDef {
+
+	pid_disable = PID_DISABLE,
+	pid_reset_output = PID_RESET_OUPUT,
+	pid_hold_output = PID_HOLD_OUTPUT,
+	pid_enable = PID_ENABLE,
+	pid_uninit = PID_UNINIT,
+	pid_failed = PID_FAILED
+
+} PID_StatusDef;
+
+typedef struct __PID_HandleTypeDef {
+
+	PID_StatusDef Status;
+	float Accuracy;
+	float SetValue;
+	float ActualValue;
+	float Offset;       //Offset: SetValue - ActualValue
+	float OffsetNext;
+	float OffsetLast;
+	float Kp;           //Proportion coefficient
+	float Ki;           //Integral coefficient
+	float Kd;           //Differential coefficient
+	float Integral;
+	float Uk;           //Output
+
+} PID_HandleTypeDef;
+
+/**
+ * PID Configuration
+ * */
+void PID_Init(PID_HandleTypeDef *pid, float kp, float ki, float kd);
+void PID_DeInit(PID_HandleTypeDef *pid);
+void PID_SetParam(PID_HandleTypeDef *pid, float kp, float ki, float kd);
+void PID_ResetParam(PID_HandleTypeDef *pid, float kp, float ki, float kd);
+void PID_SetAccuracy(PID_HandleTypeDef *pid, float accuracy);
+
+/**
+ * PID Algorithms
+ * */
+float PID_GetOffsetModul(float num);
+float PID_Model_Positional(PID_HandleTypeDef *pid, float target);
+float PID_Model_Incremental(PID_HandleTypeDef *pid, float target);
+float PID_Model_IntegralSeparation(PID_HandleTypeDef *pid, float target,
+		float moffset);
+float PID_Model_Anti_Windup(PID_HandleTypeDef *pid, float target, float umax,
+		float umin, float sepedge);
+float PID_Model_TrapezoidalIntegral(PID_HandleTypeDef *pid, float target,
+		float umax, float umin, float sepedge);
+float PID_Model_VariableIntegral(PID_HandleTypeDef *pid, float target,
+		float lowedge, float highedge);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ lib/pid.c
@@ -0,0 +1,347 @@
+#include "pid.h"
+
+/**
+ * @brief:  get modulus of a double
+ *          the same function as fabs() in "math.h"
+ *          but I don't need all functions in it
+ * @return: modulus of the param
+ * */
+float PID_GetOffsetModul(float num) {
+
+#ifndef _MATH_H_
+	if (num >= 0.0)
+		return num;
+	else
+		return -num;
+#endif
+
+#ifdef _MATH_H_
+	return fabs(num);
+#endif
+}
+
+/**
+ * @brief: Init all params in pid
+ *         Always call the function before use pid
+ *         Some of the params with be set to default
+ * @call:  Before all pid related functions
+ * */
+void PID_Init(PID_HandleTypeDef *pid, float kp, float ki, float kd) {
+
+	pid->Status = pid_enable;
+	pid->Accuracy = PID_DEFAULT_ACCURACY;
+	pid->SetValue = 0.0;
+	pid->ActualValue = 0.0;
+	pid->Offset = 0.0;
+	pid->OffsetNext = 0.0;
+	pid->OffsetLast = 0.0;
+	pid->Integral = 0.0;
+	pid->Kp = kp;
+	pid->Ki = ki;
+	pid->Kd = kd;
+	pid->Uk = 0.0;
+
+}
+/**
+ * @brief: Deinit pid
+ *         Set All params to default
+ * @call:  After pid is unneeded
+ * */
+void PID_DeInit(PID_HandleTypeDef *pid) {
+
+	pid->Status = pid_uninit;
+	pid->SetValue = 0.0;
+	pid->ActualValue = 0.0;
+	pid->Offset = 0.0;
+	pid->OffsetNext = 0.0;
+	pid->OffsetLast = 0.0;
+	pid->Kp = 0.0;
+	pid->Ki = 0.0;
+	pid->Kd = 0.0;
+	pid->Integral = 0.0;
+	pid->Uk = 0.0;
+
+}
+
+/**
+ * @brief: Set pid related params
+ *         Set Kp to kp
+ *         Set Ki to ki
+ *         Set Kd to kd
+ * @call:  If it is needed to set params
+ * */
+void PID_SetParam(PID_HandleTypeDef *pid, float kp, float ki, float kd) {
+	if (pid->Status == pid_enable) {
+			pid->Kp = kp;
+			pid->Ki = ki;
+			pid->Kd = kd;
+		} else if (pid->Status == pid_disable) {
+			PID_Init(pid, kp, ki, kd);
+			/* CODE TO SHOW MSG ABOUT PID IS DISABLE */
+
+		} else if (pid->Status == pid_uninit) {
+			PID_Init(pid, kp, ki, kd);
+			/* CODE TO SHOW MSG ABOUT PID IS UNINIT */
+
+		} else {
+			/* SET PARAMS WITH OUT OTHER OPERATION */
+			pid->Kp = kp;
+			pid->Ki = ki;
+			pid->Kd = kd;
+		}
+}
+
+/**
+ * @brief: Reset pid related params
+ *         Set Kp to kp
+ *         Set Ki to ki
+ *         Set Kd to kd
+ * @call:  If it is needed to reset params
+ * */
+void PID_ResetParam(PID_HandleTypeDef *pid, float kp, float ki, float kd) {
+
+	PID_SetParam(pid, kp, ki, kd);
+
+}
+
+/**
+ * @brief: set PID accuracy
+ * @param accuracy: desired accuracy
+ * */
+void PID_SetAccuracy(PID_HandleTypeDef *pid, float accuracy) {
+	pid->Accuracy = accuracy;
+}
+
+/**
+ * @brief: positional pid
+ * @param pid:     pid
+ * @param target:  target value
+ * */
+float PID_Model_Positional(PID_HandleTypeDef *pid, float target) {
+
+	if (pid->Status == pid_enable) {
+
+		pid->SetValue = target;
+		pid->Offset = (pid->SetValue) - (pid->ActualValue);
+		pid->Integral += pid->Offset;
+		pid->Uk = (pid->Kp * pid->Offset) + (pid->Ki * pid->Integral)
+				+ pid->Kd * (pid->Offset - pid->OffsetLast);
+		pid->OffsetLast = pid->Offset;
+		pid->ActualValue = pid->Uk * 1.0;
+
+		return pid->ActualValue;
+	}
+
+	else {
+		return (float) pid->Status;
+	}
+}
+
+/**
+ * @brief: incremental pid
+ * @param pid:     pid
+ * @param target:  target value
+ * */
+float PID_Model_Incremental(PID_HandleTypeDef *pid, float target) {
+	if (pid->Status == pid_enable) {
+
+		float increment = 0.0;
+		pid->SetValue = target;
+		pid->Offset = (pid->SetValue) - (pid->ActualValue);
+		increment = pid->Kp * (pid->Offset - pid->OffsetNext)
+				+ pid->Ki * pid->Offset
+				+ pid->Kd
+						* (pid->Offset - 2 * pid->OffsetNext + pid->OffsetLast);
+		pid->Uk += increment;
+		pid->ActualValue = pid->Uk * 1.0;
+		pid->OffsetLast = pid->OffsetNext;
+		pid->OffsetNext = pid->Offset;
+
+		return pid->ActualValue;
+
+	} else {
+		return (float) pid->Status;
+	}
+}
+
+/**
+ * @brief: integral separation pid
+ * @param pid:        pid
+ * @param target:     target value
+ * @param moffset:    max offset
+ * */
+float PID_Model_IntegralSeparation(PID_HandleTypeDef *pid, float target,
+		float moffset) {
+	if (pid->Status == pid_enable) {
+
+		uint8_t flag = 1;
+
+		pid->SetValue = target;
+		pid->Offset = (pid->SetValue) - (pid->ActualValue);
+
+		if ((float) GetModul(pid->Offset) > moffset) {
+			flag = 0;
+		} else {
+			flag = 1;
+			pid->Integral += pid->Offset;
+		}
+		pid->Uk = (pid->Kp * pid->Offset) + (flag * pid->Ki * pid->Integral)
+				+ pid->Kd * (pid->Offset - pid->OffsetLast);
+
+		pid->OffsetLast = pid->Offset;
+		pid->ActualValue = pid->Uk * 1.0;
+
+		return pid->ActualValue;
+
+	} else {
+		return (float) pid->Status;
+	}
+}
+
+/**
+ * @brief: trapezoidal integral pid
+ * @param pid:        pid
+ * @param target:     target value
+ * @param umax:       max integral offset of integral
+ * @param umin:       min integral offset of integral
+ * @param sepedge:    separation edge
+ * */
+float PID_Model_Anti_Windup(PID_HandleTypeDef *pid, float target, float umax,
+		float umin, float sepedge) {
+	if (pid->Status == pid_enable) {
+
+		uint8_t flag = 0;
+		pid->SetValue = target;
+		pid->Offset = pid->SetValue - pid->ActualValue;
+
+		if (pid->ActualValue > umax) {
+			if (GetModul(pid->Offset) > sepedge) {
+				flag = 0;
+			} else {
+				flag = 1;
+				if (pid->Offset < 0) {
+					pid->Integral += pid->Offset;
+				}
+			}
+		} else if (pid->ActualValue < umin) {
+			if (GetModul(pid->Offset) > sepedge) {
+				flag = 0;
+			} else {
+				flag = 1;
+				if (pid->Offset > 0) {
+					pid->Integral += pid->Offset;
+				}
+			}
+		} else {
+			if (GetModul(pid->Offset) > sepedge) {
+				flag = 0;
+			} else {
+				flag = 1;
+				pid->Integral += pid->Offset;
+			}
+		}
+
+		pid->Uk = (pid->Kp * pid->Offset) + (flag * pid->Ki * pid->Integral)
+				+ pid->Kd * (pid->Offset - pid->OffsetLast);
+		pid->OffsetLast = pid->Offset;
+		pid->ActualValue = pid->Uk * 1.0;
+
+		return pid->ActualValue;
+
+	} else {
+		return (float) pid->Status;
+	}
+}
+
+/**
+ * @brief: trapezoidal integral pid
+ * @param pid:        pid
+ * @param target:     target value
+ * @param umax:       max integral offset
+ * @param umin:       min integral offset
+ * @param sepedge:    separation edge
+ * */
+float PID_Model_TrapezoidalIntegral(PID_HandleTypeDef *pid, float target,
+		float umax, float umin, float sepedge) {
+	if (pid->Status == pid_enable) {
+
+		uint8_t flag = 0;
+		pid->SetValue = target;
+		pid->Offset = pid->SetValue - pid->ActualValue;
+
+		if (pid->ActualValue > umax) {
+			if (GetModul(pid->Offset) > sepedge) {
+				flag = 0;
+			} else {
+				flag = 1;
+				if (pid->Offset < 0) {
+					pid->Integral += pid->Offset;
+				}
+			}
+		} else if (pid->ActualValue < umin) {
+			if (GetModul(pid->Offset) > sepedge) {
+				flag = 0;
+			} else {
+				flag = 1;
+				if (pid->Offset > 0) {
+					pid->Integral += pid->Offset;
+				}
+			}
+		} else {
+			if (GetModul(pid->Offset) > sepedge) {
+				flag = 0;
+			} else {
+				flag = 1;
+				pid->Integral += pid->Offset;
+			}
+		}
+
+		pid->Uk = (pid->Kp * pid->Offset) + (flag * pid->Ki * pid->Integral / 2)
+				+ pid->Kd * (pid->Offset - pid->OffsetLast);
+		pid->OffsetLast = pid->Offset;
+		pid->ActualValue = pid->Uk * 1.0;
+
+		return pid->ActualValue;
+
+	} else {
+		return (float) pid->Status;
+	}
+}
+
+/**
+ * @brief: variable integral pid
+ * @param pid:        pid
+ * @param target:     target value
+ * @param lowedge:    lower limit of variable integral
+ * @param highedge:   upper limit of variable integral
+ * */
+float PID_Model_VariableIntegral(PID_HandleTypeDef *pid, float target,
+		float lowedge, float highedge) {
+	if (pid->Status == pid_enable) {
+
+		float Ki2;
+		pid->SetValue = target;
+		pid->Offset = pid->SetValue - pid->ActualValue;
+
+		if ((float) GetModul(pid->Offset) > highedge) {
+			Ki2 = 0.0;
+		} else if ((float) GetModul(pid->Offset) < lowedge) {
+			Ki2 = 1.0;
+			pid->Integral += pid->Offset;
+		} else {
+			Ki2 = (highedge - (float) GetModul(pid->Offset))
+					/ (highedge - lowedge);
+			pid->Integral += pid->Offset;
+		}
+		pid->Uk = pid->Kp * pid->Offset + Ki2 * pid->Ki * pid->Integral
+				+ pid->Kd * (pid->Offset - pid->OffsetLast);
+
+		pid->OffsetLast = pid->Offset;
+		pid->ActualValue = pid->Uk * 1.0;
+
+		return pid->ActualValue;
+
+	} else {
+		return (float) pid->Status;
+	}
+}
